8441a66334a57ca69e959366b655ba789bad00ee
Rithi-90-lgtm <kit26.csbs43@gmail.com>
2024-09-11 11:33:16 +0530

    Add files via upload

---
+++ portfolio/p.js
@@ -0,0 +1,31 @@
+var typed=new Typed(".auto-type",{
+    strings:[" student", "learner about web desining" ],
+    typespeed:778,
+    backspeed:1000,
+    loop:true,
+
+})
+
+
+  function watsapp(){
+    window.open(" https://chat.whatsapp.com/IFOOpmM6MlABqe7mY1uMCr")
+
+  }
+  
+    function link(){
+        window.open("https://www.linkedin.com/in/rithika-r-732611261/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")}
+        
+  
+            
+            function li(){
+              window.open("https://github.com/Rithi-90-lgtm")
+            }
+            function lo(){
+              window.open("https://www.google.com/maps/@13.0220032,80.19968,11z?entry=ttu")
+            }
+            function log(){
+              window.open("tel:9025176823")
+            
+            }
+            function logi(){
+              window.open(" mailto:rithikahema2006@gmail.com")}
